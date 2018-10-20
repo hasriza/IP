162325bdcba632ba424dc7c33fc6b905efb6491e
hasriza <hasanriza12@gmail.com>
2018-10-20 07:02:06 +0530

    current

---
+++ volunteer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TetraFree | Volunteer</title>
+	<link rel="stylesheet" href="style/style.css" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Recycling TetraPack cartons into finished products">
+	<meta name="keywords" content="Recycle, TetraPack, TetraPack Benches">
+</head>
+<body>
+
+	<!-- Main header, common on all pages, replace h1 by logo -->
+	<?php require "head.html" ?>
+	<div id="section">
+	<header>
+		<h1>Volunteer</h1>
+		<h3>Campaign For The Change</h3>
+	</header>
+
+	<section>
+		<article id='new'>
+			<h2>Ongoing Campaigns</h2>
+			<ul>
+				<li>Campaign 1
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. </p>
+					<a href="@">Join The Campaign</a>
+				</li>
+				<li>Campaign 2
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. </p>
+					<a href="@">Join The Campaign</a>
+				</li>
+				<li>Campaign 2
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. </p>
+					<a href="@">Join The Campaign</a>
+				</li>
+			</ul>
+		</article>
+
+		<article>
+			<h2>Conduct A Campaign</h2>
+			<a href="camp_form.html">Start A Campaign</a>
+		</article>
+	</section>
+</div>
+	<?php require "foot.html" ?>
+	
+<script src="scripts/script.js" type="text/javascript"></script>
+</body>
+</html>
+
